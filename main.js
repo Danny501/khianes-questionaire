e14b8724c985558931bdbd71bf06626b485e9858
Danny501 <55329264+Danny501@users.noreply.github.com>
2020-10-17 22:54:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 
-const token = "NzY3MTIzNDYxMTg4MzU0MDcw.X4tVZQ.MilJqhulNlP1x7wCiSyAow2UBUI";
+const token = "CENSORED";
 
 const prefix = "!";
 
 bot.on('ready', () => {
     console.log('Yes ma brudda!');
     let onlineguild = bot.guilds.cache.get('726408655775334430')
-    let onlinechannel = onlineguild.channels.cache.get('726408655775334433')
+    let onlinechannel = onlineguild.channels.cache.get('767141355361009684')
     let questiononeembed = new discord.MessageEmbed()
         .setTitle("Question One")
         .setDescription("Who was Prime Minister of the United Kingdom at the time of the Afgan war?\n```1: Tony Blair```\n```2: Theresa May```\n```3: Borris Johnson```\n```4: David Cameron```\n```5: Margret Thatcher```")
@@ -36,10 +36,45 @@ bot.on('message', message => {
             }
         break;
         case '2':
-            if (args[1] === 'Three') {
+            if (args[1] === 'Four') {
                 message.channel.send('Correct! You probably know this song from your childhood!')
+                let questionthreeembed = new discord.MessageEmbed()
+                    .setTitle('Question Three')
+                    .setDescription('When was the song \"Big Rich Town\" by 50 Cent officially released?\n```1: 2016```\n```2: 2013```\n```3: 2018```\n```4: 2014```\n```5: 2019```')
+                    .setTimestamp()
+                message.channel.send(questionthreeembed)
             } else {
                 message.channel.send('I\'m so disappointed right now...')
+            }
+        break;
+        case '3':
+            if (args[1] === 'Four') {
+                message.channel.send('Correct! It was released as the theme song for the show Power on Netflix!')
+                let questionfourembed = new discord.MessageEmbed()
+                    .setTitle('Question Four')
+                    .setDescription('In what year did World War One end?\n```1: 1918```\n```2: 1916```\n```3: 1917```\n```4: 1919```\n```5: 1920```')
+                    .setTimestamp()
+                message.channel.send(questionfourembed)
+            } else {
+                message.channel.send('Cmon, even I know this!')
+            }
+        break;
+        case '4':
+            if (args[1] === 'One') {
+                message.channel.send('Correct! World War One began in 1914 and ended in 1918.')
+                let questionfiveembed = new discord.MessageEmbed()
+                    .setTitle('Question Five')
+                    .setDescription('In what year did Pear Harbour happen?\n```1: 1941```\n```2: 1942```\n```3: 1940```\n```4: 1943```\n```5: 1944```')
+                message.channel.send(questionfiveembed)
+            } else {
+                message.channel.send('Nein...')
+            }
+        break;
+        case '5':
+            if (args[1] === 'One') {
+                message.channel.send('Correct. You have reached the end of the questionaire... for now at least. More questions coming soon!')
+            } else {
+                message.channel.send('Incorrect. Try again.')
             }
         break;
     }
